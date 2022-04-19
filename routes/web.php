abb1f57fe25527a022594b2f86a7c9465b94055f
Asmi <shamsudeenasmi96@gmail.com>
2022-04-19 05:34:30 +0530

    todo blade

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,33 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/body', function(){
-    return view('dashboard.order_summery');
+    return view('dashboard.body');
+});
+
+Route::get('/addnewcategory', function(){
+    return view('dashboard.add_new_category');
+});
+Route::get('/editnewcategory', function(){
+    return view('dashboard.add_new_category_edit');
+});
+Route::get('/chart', function(){
+    return view('dashboard.charts');
+});
+Route::get('/chat', function(){
+    return view('dashboard.chats');
+});
+Route::get('/datatable', function(){
+    return view('dashboard.datatable');
+});
+Route::get('/datatable2', function(){
+    return view('dashboard.datatable2');
+});
+Route::get('/order_semmary', function(){
+    return view('dashboard.order_summery_client');
+});
+Route::get('/order_semmary_merchain', function(){
+    return view('dashboard.order_summery_merchain');
+});
+Route::get('/todo', function(){
+    return view('dashboard.todo');
 });
